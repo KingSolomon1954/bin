c45ef45c6de542339fd5f016e6735bf6714085a6
KingSolomon <KingSolomon1954@gmail.com>
2020-06-28 15:44:18 -0700

    auto start/place four terminals/bash

---
+++ cinn-windows.bash
@@ -0,0 +1,6 @@
+
+gnome-terminal --geometry=80x30+2590+0   --title="1 Upper Left"
+gnome-terminal --geometry=80x30+2590+540 --title="2 Lower Left"
+
+gnome-terminal --geometry=70x30+3300+0   --title="3 Upper Right"
+gnome-terminal --geometry=70x30+3300+540 --title="4 Lower Right"
