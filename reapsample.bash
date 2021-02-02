ffa6c22366cd7a0c8bb3280b3568aed343a55a23
hsolomon <hsolomon@viasat.com>
2021-02-02 12:17:28 -0800

    work-in-progress

--- reapsample.bash
+++ reapsample.bash
@@ -5,7 +5,7 @@ cntr=docker
 if type -t podman 1>&2 > /dev/null; then
     cntr=podman
 fi
-${cntr} exec sgn-gcc9 ps aux | grep SampleApp | colrm 17 | colrm 1 11 > ~/tmp/pids
+${cntr} exec sgn-gcc9 ps aux | grep SampleApp | awk '{ print $2 }' > ~/tmp/pids
 if [ -s ~/tmp/pids ]; then
     ${cntr} exec sgn-gcc9 kill -s SIGTERM $(cat ~/tmp/pids)
 fi
