5bfdaced740d701bd25506873d2e35959970856a
KingSolomon <KingSolomon1954@gmail.com>
2021-02-02 02:10:14 -0800

    use ctnr, fix columns

--- reapsample.bash
+++ reapsample.bash
@@ -5,8 +5,8 @@ cntr=docker
 if type -t podman 1>&2 > /dev/null; then
     cntr=podman
 fi
-docker exec sgn-gcc9 ps aux | grep SampleApp | colrm 16 | colrm 1 10 > ~/tmp/pids
+${cntr} exec sgn-gcc9 ps aux | grep SampleApp | colrm 17 | colrm 1 11 > ~/tmp/pids
 if [ -s ~/tmp/pids ]; then
-    docker exec sgn-gcc9 kill -s SIGTERM $(cat ~/tmp/pids)
+    ${cntr} exec sgn-gcc9 kill -s SIGTERM $(cat ~/tmp/pids)
 fi
 rm -f ~/tmp/pids
